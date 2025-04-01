e738f4fc202c7a13f6958afaee7a8d4f10806419
Jose Luis Cáceres <caceresjoselu@gmail.com>
2025-04-01 12:45:26 +0200

    iconos

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,19 @@
 import Link from 'next/link';
 import './globals.css';
+import { IoCalendarNumber } from "react-icons/io5";
+import { GiTeacher } from "react-icons/gi";
+import { PiStudentFill } from "react-icons/pi";
+import { FaSchoolFlag } from "react-icons/fa6";
 
 const Navigation = () => {
   return (
     <nav className="navigation">
       <ul>
+        <li><FaSchoolFlag size={100} /></li>
+        <li><IoCalendarNumber size={100} /></li>
+        <li><GiTeacher size={100} /></li>
+        <li><PiStudentFill size={100} /></li>
+
         <li><Link href="/pages/dashboard">Dashboard</Link></li>
         <li><Link href="/pages/alumnos">Alumnos</Link></li>
         <li><Link href="/pages/contactos">Contactos</Link></li>
