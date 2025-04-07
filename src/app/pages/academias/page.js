0cc5339cbe9be153beac92629d66da934fb3b18d
Jose Luis Cáceres <caceresjoselu@gmail.com>
2025-04-07 13:31:49 +0200

    academia

--- src/app/pages/academias/page.js
+++ src/app/pages/academias/page.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 const AcademiasPage = () => {
   return (
-    <div>
-      <h1>Academias</h1>
-      <p>Academia Alhamar</p>
-      <p>Academia Zaidín</p>
+    <div style={{ display: 'flex', gap: '18%', marginTop: "10%", marginLeft: "3%" }}>
+      <h1>Academia</h1>
+      <h1>Dirección</h1>
+      <h1>Teléfono</h1>
+      <h1>Nº Alumnos</h1>
     </div>
   );
 };
