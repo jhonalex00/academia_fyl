b563e7fc013de0bca72b36ed1017d3c998988597
Mónica <monica.mar.zam.v@gmail.com>
2025-04-22 23:49:26 +0200

    Estructura alumnos

--- src/app/pages/alumnos/page.js
+++ src/app/pages/alumnos/page.js
@@ -1,16 +1,135 @@
 'use client';
 
 import React from 'react';
+import { FaInfoCircle } from "react-icons/fa";
+import { MdContactPhone } from "react-icons/md";
+import { Button } from "@/components/ui/button";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+// import { useForm } from "react-hook-form"
+// import {
+//   Form,
+//   FormField,
+//   FormItem,
+//   FormLabel,
+//   FormControl,
+// } from "@/components/ui/form"
+
+import {
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogDescription,
+  DialogFooter,
+  DialogClose,
+} from "@/components/ui/dialog";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Input } from "@/components/ui/input";
+
+
+
+
 
 const AlumnosPage = () => {
-  const handleClick = () => {
-    alert('¡Botón clickeado en la página de Alumnos!');
-  };
-
   return (
-    <div>
+    <div >
+      <div className="flex justify-between">
+        <Input className="max-w-60" type="text" placeholder="Buscar alumno" />
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="default">Nuevo alumno</Button>
+          </DialogTrigger>
+
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Registrar nuevo alumno</DialogTitle>
+            </DialogHeader>
+
+            {/* Aquí podrías poner un formulario más adelante */}
+
+          </DialogContent>
+        </Dialog>
+      </div>
+      <div className="mt-20 flex justify-center">
+        <Table className="text-center">
+          <TableHeader className="bg-neutral-100">
+            <TableRow >
+              <TableHead>Avatar</TableHead>
+              <TableHead>Nombre</TableHead>
+              <TableHead>Curso</TableHead>
+              <TableHead>Ciclo</TableHead>
+              <TableHead>Horas</TableHead>
+              <TableHead>Contacto</TableHead>
+              <TableHead>Detalles</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell className="flex justify-center"> 
+                <Avatar>
+                  <AvatarImage src="https://www.aurubis.com/.resources/aurubis-light-module/webresources/assets/img/image-avatar-avatar-fallback.svg" alt="@shadcn" />
+                  <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+              </TableCell>
+              <TableCell>Manolito</TableCell>
+              <TableCell>1º</TableCell>
+              <TableCell>ESO</TableCell>
+              <TableCell>4</TableCell>
+              <TableCell>
+                <Dialog>
+                  <DialogTrigger asChild>
+                    <button className="text-blue-600 cursor-pointer">
+                      <MdContactPhone size={18} />
+                    </button>
+                  </DialogTrigger>
+
+                  <DialogContent>
+                    <DialogHeader>
+                      <DialogTitle>Contacto del alumno</DialogTitle>
+                      <DialogDescription>
+                        Aquí iría la información de contacto como teléfono o email.
+                      </DialogDescription>
+                    </DialogHeader>
+                  </DialogContent>
+                </Dialog>
+              </TableCell>
+              <TableCell>
+              <Dialog>
+                <DialogTrigger asChild>
+                  <button className="text-blue-600 cursor-pointer">
+                    <FaInfoCircle size={18} />
+                  </button>
+                </DialogTrigger>
+
+                <DialogContent>
+                  <DialogHeader>
+                    <DialogTitle>Detalles del alumno</DialogTitle>
+                    <DialogDescription>
+                      Aquí puedes ver información adicional como dirección, notas o historial académico.
+                    </DialogDescription>
+                  </DialogHeader>
+                </DialogContent>
+              </Dialog>
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </div>
     </div>
   );
 };
 
 export default AlumnosPage;
+
