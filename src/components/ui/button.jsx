6f1d4a7d923d8fa4532c1fe435e6ae29a1346471
Mónica <monica.mar.zam.v@gmail.com>
2025-05-06 13:09:25 +0200

    .

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -16,7 +16,7 @@ const buttonVariants = cva(
         outline:
           "border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
         secondary:
-          "bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80",
+          "bg-[#FF0C4E] text-[#F9CDD8] shadow-xs hover:bg-[#F9CDD8] hover:text-[#FF0C4E]",
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
