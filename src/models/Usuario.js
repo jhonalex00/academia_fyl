431b0f1b8b73fabc73a50d7362c478ae28a2e517
jhon alex caro <jcc24gr042@student.esgerencia.com>
2025-06-14 15:16:22 +0200

    user

--- src/models/Usuario.js
+++ src/models/Usuario.js
@@ -1,31 +1,35 @@
-const { DataTypes } = require('sequelize');
-const { sequelize } = require('../db/config');
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("../db/config");
 
-const Usuario = sequelize.define('Usuario', {
-  iduser: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-    field: 'iduser'
+const Usuario = sequelize.define(
+  "Usuario",
+  {
+    iduser: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      field: "iduser",
+    },
+    nombre: {
+      type: DataTypes.STRING(50),
+      allowNull: true,
+      field: "name",
+    },
+    password: {
+      type: DataTypes.STRING(50),
+      allowNull: true,
+      field: "password",
+    },
+    idacademy: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      field: "idacademy",
+    },
   },
-  nombre: {
-    type: DataTypes.STRING(50),
-    allowNull: true,
-    field: 'name'
-  },
-  password: {
-    type: DataTypes.STRING(50),
-    allowNull: true,
-    field: 'password'
-  },
-  idacademy: {
-    type: DataTypes.INTEGER,
-    allowNull: true,
-    field: 'idacademy'
+  {
+    tableName: "usuarios", // ✅ aquí está la corrección
+    timestamps: false,
   }
-}, {
-  tableName: 'users',
-  timestamps: false
-});
+);
 
 module.exports = Usuario;
